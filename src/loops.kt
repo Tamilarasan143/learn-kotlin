e07e5a02dff3d1482f67366c42956e8d7ba3d67a
tamilarasan elumalai <tamilarasan@lynexsoft.com>
2025-01-09 22:06:14 +0530

    learning while and do-while in loops

--- src/loops.kt
+++ src/loops.kt
@@ -1,5 +1,7 @@
 fun main(){
     //ranges, you can create a for loop that iterates over numbers 1 to 5 and prints the number each time.
+    var i =10
+    var sum = 0
     for (number in 1..5) {
         // number is the iterator and 1..5 is the range
         println(number)
@@ -15,4 +17,16 @@ fun main(){
         if (w.startsWith("l"))
             println(w)
     }
+    while (i != 0 ){
+        println("the value of i = $i before")
+        sum++
+        i--
+        println("the value of i = $i after")
+    }
+    println("the final value of sum = $sum")
+    println("do-while")
+    do{
+        println("the value sum in do - while sum =$sum ")
+        --sum
+    }while(sum != 0)
 }
